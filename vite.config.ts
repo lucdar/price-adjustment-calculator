f2a9cf0ce195c6d71303f0c68bdeb8db018db5a0
Lucas Clark <lucas.clark@berkeley.edu>
2025-03-21 20:43:15 -0700

    edit config for deployment to laclark.me

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/price-adjustment-calculator/",
+  base: "/tools/price-adjustment-calculator/",
   plugins: [react()],
 });
